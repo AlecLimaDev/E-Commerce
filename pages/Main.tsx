6f7138dba64a193b711b916cf905f66df8bb304a
Alec Lima <aleclimadev@gmail.com>
2023-02-25 14:54:56 -0300

    feat: main request

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -1,57 +1,59 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/no-children-prop */
+/* eslint-disable react/jsx-key */
+import React, { useEffect, useState } from "react";
 
 import {
-    MainContainer,
-    ContentContainer,
-    Products,
-    TextProducts,
-    PriceProducts,
-    DescriptionProducts,
-    BuyProducts
-} from '../styles/style'
+  MainContainer,
+  ContentContainer,
+  Products,
+  TextProducts,
+  PriceProducts,
+  DescriptionProducts,
+  BuyProducts,
+} from "../styles/style";
 
-import { FaCcVisa } from 'react-icons/fa'
+import { FaCcVisa } from "react-icons/fa";
+import { api } from "../src/services/api";
 
+type Props = {
+  data?: Array<string> | Array<number>
+};
 
+const Main = ({ data }: Props) => {
+    const [cart, setCart] = useState([]);
 
-const Main: React.FC = () => {
-    return (
-        <>
-            <MainContainer>
-                <ContentContainer>
-                    <Products />
-                    <TextProducts>
-                        Apple Watch modelo de exemplo
-                        <PriceProducts>
-                            R$754
-                        </PriceProducts>
-                    </TextProducts>
-                    <DescriptionProducts>
-                        Descrição do produto
-                    </DescriptionProducts>
-                    <BuyProducts>
-                        <FaCcVisa />  COMPRAR
-                    </BuyProducts>
-                </ContentContainer>
+  const fetchData = () => {
+    api.get("/cart").then((response) => setCart(response.data));
+  }
 
-                <ContentContainer>
-                    <Products />
-                    <TextProducts>
-                        Apple IphoneX 128GB
-                        <PriceProducts>
-                            R$754
-                        </PriceProducts>
-                    </TextProducts>
-                    <DescriptionProducts>
-                        Descrição do produto
-                    </DescriptionProducts>
-                    <BuyProducts>
-                        <FaCcVisa />  COMPRAR
-                    </BuyProducts>
-                </ContentContainer>
-            </MainContainer>
-        </>
-    )
-}
+  useEffect(() => {
+    fetchData()
+  }, []);
 
-export default Main
+  return (
+    <>
+      <MainContainer>
+      {cart.map((item) => (
+         <ContentContainer>
+         <Products>
+           <img src="https://picsum.photos/120/130" alt="foto" />
+         </Products>
+         <TextProducts>
+           <PriceProducts>R$ </PriceProducts>
+         </TextProducts>
+         <DescriptionProducts></DescriptionProducts>
+         <BuyProducts>
+           {" "}
+           COMPRAR
+           <FaCcVisa></FaCcVisa>
+         </BuyProducts>
+       </ContentContainer>
+      ))}
+      {cart.length === 0 && <div>Carrinho vazio</div>}
+      </MainContainer>
+    </>
+  );
+};
+
+export default Main;
